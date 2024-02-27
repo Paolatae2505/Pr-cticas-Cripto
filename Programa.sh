8178fdaa6af2592bfee5e71c93195f10b127b93d
Paola V.B <Paola_VB@ciencias.unam.mx>
2024-02-26 18:05:43 -0600

    Modificación

--- Programa.sh
+++ Programa.sh
@@ -1,65 +1,35 @@
 #!/bin/bash
 
-# Función para obtener información WHOIS del dominio
-obtener_info_whois() {
-    whois "$1"
-}
-
-# Función para obtener información DNS
-obtener_info_dns() {
-    nslookup "$1"
-}
-
-# Función para realizar un ping al dominio
-obtener_info_ping() {
-    ping -c 4 "$1"
-}
-
-# Función para realizar un traceroute al dominio
-obtener_info_traceroute() {
-    traceroute "$1"
-}
-
-obtener_info_nmap() {
-    nmap "$1"
-}
-
-# Función para guardar información en un archivo
-guardar_info_archivo() {
-    echo "$2" >> "$1"
-}
-
+echo "-----------------------------------"
 echo "Ingrese el dominio o IP a analizar:"
+echo "-----------------------------------"
 read dominio
 
 archivo_salida="info_relevante.txt"
+
+# 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
 
 # Limpiar archivo de salida
 > "$archivo_salida"
 
 # Obtener información WHOIS y guardarla en el archivo de salida
-info_whois=$(obtener_info_whois "$dominio")
-guardar_info_archivo "$archivo_salida" "=== Información WHOIS ==="
-guardar_info_archivo "$archivo_salida" "$info_whois"
+echo "=== Información WHOIS ===" >> "$archivo_salida"
+whois "$dominio" | grep -E 'Domain Name:|Creation Date:|Updated Date:|Registry Expiry Date:|Registrant Name:|Registrant Organization:|Registrant Street:|Registrant City:|Registrant State/Province:|Registrant Postal Code:|Registrant Country:|Registrant Phone:|Registrant Email:' >> "$archivo_salida"
 
 # Obtener información DNS y guardarla en el archivo de salida
-info_dns=$(obtener_info_dns "$dominio")
-guardar_info_archivo "$archivo_salida" "=== Información DNS ==="
-guardar_info_archivo "$archivo_salida" "$info_dns"
+echo "=== Información DNS ===" >> "$archivo_salida"
+nslookup "$dominio" | grep -E 'Name:|Address:' >> "$archivo_salida"
 
 # Realizar un ping y guardar la información en el archivo de salida
-info_ping=$(obtener_info_ping "$dominio")
-guardar_info_archivo "$archivo_salida" "=== Resultado del Ping ==="
-guardar_info_archivo "$archivo_salida" "$info_ping"
+echo "=== Resultado del Ping ===" >> "$archivo_salida"
+ping -c 4 "$dominio" >> "$archivo_salida"
 
 # Realizar traceroute y guardar la información en el archivo de salida
-info_traceroute=$(obtener_info_traceroute "$dominio")
-guardar_info_archivo "$archivo_salida" "=== Resultado del Traceroute ==="
-guardar_info_archivo "$archivo_salida" "$info_traceroute"
+echo "=== Resultado del Traceroute ===" >> "$archivo_salida"
+traceroute "$dominio" >> "$archivo_salida"
 
 # Escanear puertos utilizando Nmap y guardar la información en el archivo de salida
-info_nmap=$(obtener_info_nmap "$dominio")
-guardar_info_archivo "$archivo_salida" "=== Resultado del Escaneo de Puertos con Nmap ==="
-guardar_info_archivo "$archivo_salida" "$info_nmap"
+echo "=== Resultado del Escaneo de Puertos con Nmap ===" >> "$archivo_salida"
+nmap -Pn -p- --open -T4 "$dominio" >> "$archivo_salida"
 
 echo "Se ha recopilado la información relevante en el archivo $archivo_salida."
