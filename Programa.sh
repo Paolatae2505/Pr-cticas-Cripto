e1de55e98d941a8afc5cf067f4d33bc24d29bb53
Paola V.B <Paola_VB@ciencias.unam.mx>
2024-02-26 21:34:45 -0600

    Cambios

--- Programa.sh
+++ Programa.sh
@@ -1,35 +1,64 @@
 #!/bin/bash
 
-echo "-----------------------------------"
-echo "Ingrese el dominio o IP a analizar:"
-echo "-----------------------------------"
-read dominio
+# Solicitar al usuario que ingrese el nombre del dominio
+read -p "Ingresa el nombre de dominio: " domain
 
-archivo_salida="info_relevante.txt"
+# Obtener información WHOIS
+whois_info=$(whois $domain)
 
-# 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
+# Obtener la IP pública y sus segmentos usando nslookup
+ip_info=$(nslookup $domain)
 
-# Limpiar archivo de salida
-> "$archivo_salida"
+# Extraer la IP pública y sus segmentos de la salida de nslookup
+ip_address=$(echo "$ip_info" | awk '/^Address: / { print $2 }')
+ip_segment=$(echo "$ip_address" | cut -d'.' -f1-3)
 
-# Obtener información WHOIS y guardarla en el archivo de salida
-echo "=== Información WHOIS ===" >> "$archivo_salida"
-whois "$dominio" | grep -E 'Domain Name:|Creation Date:|Updated Date:|Registry Expiry Date:|Registrant Name:|Registrant Organization:|Registrant Street:|Registrant City:|Registrant State/Province:|Registrant Postal Code:|Registrant Country:|Registrant Phone:|Registrant Email:' >> "$archivo_salida"
+# Obtener el registro de disponibilidad
+availability=$(whois $domain | grep -i "domain status" | awk '{print $3}')
 
-# Obtener información DNS y guardarla en el archivo de salida
-echo "=== Información DNS ===" >> "$archivo_salida"
-nslookup "$dominio" | grep -E 'Name:|Address:' >> "$archivo_salida"
+# Imprimir información deseada
+echo "----------------------------------------------"
+echo "-------- INFORMACIÓN DEL DOMINIO -------------"
+echo "----------------------------------------------"
 
-# Realizar un ping y guardar la información en el archivo de salida
-echo "=== Resultado del Ping ===" >> "$archivo_salida"
-ping -c 4 "$dominio" >> "$archivo_salida"
+echo "$whois_info" | grep -E 'Domain Name|Created On|Last Updated On|Expiration Date'
 
-# Realizar traceroute y guardar la información en el archivo de salida
-echo "=== Resultado del Traceroute ===" >> "$archivo_salida"
-traceroute "$dominio" >> "$archivo_salida"
+echo "$whois_info" | grep -A3 'Registrant:' | grep -E 'Name:|City:|State:|Country:|Country Name:'
 
-# Escanear puertos utilizando Nmap y guardar la información en el archivo de salida
-echo "=== Resultado del Escaneo de Puertos con Nmap ===" >> "$archivo_salida"
-nmap -Pn -p- --open -T4 "$dominio" >> "$archivo_salida"
+echo "----------------------------------------------"
+echo "-------- INFORMACIÓN DEL PERSONAL ------------"
+echo "----------------------------------------------"
 
-echo "Se ha recopilado la información relevante en el archivo $archivo_salida."
+echo "$whois_info" | grep -A3 'Administrative Contact:' | grep -E 'Name:|City:|State:|Country:|Country Name:'
+
+echo "$whois_info" | grep -A3 'Technical Contact:' | grep -E 'Name:|City:|State:|Country:|Country Name:'
+
+echo "$whois_info" | grep -A3 'Billing Contact:' | grep -E 'Name:|City:|State:|Country:|Country Name:'
+
+echo "----------------------------------------------"
+echo "--------- CONECTIVIDAD DE RED ----------------"
+echo "----------------------------------------------"
+
+# Realizar el ping al dominio
+if ping_result=$(ping -c 4 $domain 2>&1); then
+    echo "Conexión exitosa con $domain"
+    latency=$(echo "$ping_result" | grep -oP 'round-trip.*?=' | awk '{print $4}')
+    echo "Latencia: $latency ms"
+else
+    echo "No se pudo establecer conexión con $domain"
+fi
+
+echo "----------------------------------------------"
+echo "------ INFORMACIÓN DE LA IP PÚBLICA -----------"
+echo "----------------------------------------------"
+
+echo "IP Pública: $ip_address"
+echo "Segmentos de la IP: $ip_segment"
+
+
+# Imprimir el registro de disponibilidad
+echo "----------------------------------------------"
+echo "---- REGISTRO DE DISPONIBILIDAD DEL DOMINIO ---"
+echo "----------------------------------------------"
+echo "Estado del dominio: $availability"
+
